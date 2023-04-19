7a741ef42ee147fe68a91398a3a3fa347e09d0c9
ValerioCeccarelli <42637334+ValerioCeccarelli@users.noreply.github.com>
2023-04-19 18:30:51 +0200

    favicon

--- src/index.php
+++ src/index.php
@@ -1,23 +1,34 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>MonoVattino</title>
+</head>
+<body>
+    index page</br>
+    <a href="account/login.php">Login</a></br>
+    <a href="account/register.php">Register</a>
 
-if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    echo 'ERROR 405: Method Not Allowed';
-    exit;
-}
-
-if (empty($_COOKIE['jwt'])) {
-    echo "You are not logged in!";
-} else {
-    require_once("lib/jwt.php");
-
-    $jwt = $_COOKIE['jwt'];
-    $payload = jwt_decode($jwt);
-
-    if ($payload) {
-        echo "Hello, " . $payload->username;
-    } else {
-        echo "invalid token";
+    <?php 
+    
+    if (isset($_COOKIE['jwt'])) {
+        require_once('lib/jwt.php');
+        $jwt = $_COOKIE['jwt'];
+        $jwt_payload = jwt_decode($jwt);
+        if ($jwt_payload) {
+            echo 'Hello ' . $jwt_payload->username;
+        }
+        else {
+            echo 'Hello guest, invalid jwt';
+        }
+    }else {
+        echo 'Hello guest';
     }
-}
-
-?>
+    
+    ?>
+</body>
+</html>
