f76c12332ec118a060b33a25a7ab58f432c07663
ValerioCeccarelli <42637334+ValerioCeccarelli@users.noreply.github.com>
2023-05-20 22:15:08 +0200

    utils update

--- utils/point_generator.py
+++ utils/point_generator.py
@@ -15,19 +15,20 @@ companies = [i for i in range(1, n_companies + 1)]
 
 n_points = int(input("Insert number of points: "))
 
-points = []
+scooters = []
 
 for i in range(n_points):
     lat = random.uniform(lat_start, lat_end)
     lon = random.uniform(lon_start, lon_end)
     comp = random.choice(companies)
-    points.append((lat, lon, comp))
+    bat = random.randint(1, 100)
+    scooters.append((lat, lon, bat, comp))
 
-sql_template = 'INSERT INTO scooters (latitude, longitude, battery_level, company) VALUES ({lat}, {lon}, 100, {comp});'
+sql_template = 'INSERT INTO scooters (latitude, longitude, battery_level, company) VALUES ({lat}, {lon}, {bat}, {comp});'
 
 file_name = input("Insert file name: ")
 
 with open('file_name', 'w') as f:
-    for point in points:
-        sql = sql_template.format(lat=point[0], lon=point[1], comp=point[2])
+    for scooter in scooters:
+        sql = sql_template.format(lat=scooter[0], lon=scooter[1], bat=scooter[2], comp=scooter[3])
         f.write(sql + '\n')
