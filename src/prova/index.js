af19049795654dd5f3d3f7d57b6ef1495c787eec
Mirko Magistrado Dacara <101113216+MirkoMD@users.noreply.github.com>
2023-05-28 10:43:23 +0200

    Commit prima della tempesta

--- src/prova/index.js
+++ src/prova/index.js
@@ -122,12 +122,11 @@ async function initMap(latitude, longitude) {
     map = new Map(document.getElementById("map"), {
         zoom: 14,
         center: position,
-
         // mapId: "18db44928f96d960", // default
-        // mapId: "a4960208d9b76361", // dark
-        // mapId: "7bf73a088c3484e4", // night
+        //mapId: "a4960208d9b76361", // dark
+        mapId: "7bf73a088c3484e4", // night
         // mapId: "4d28faf75cbe2224", // atlas
-        mapId: "a85cc9c21291463", // classic
+        // mapId: "a85cc9c21291463", // classic
         // mapId: "f12bf4b63529e007", // grey
         // mapId: "b00ca340d0b7980f", // light
         streetViewControl: false,
