2e3cd1c6bfed80d1f0dea2ab23508ee3dcd80f52
ValerioCeccarelli <42637334+ValerioCeccarelli@users.noreply.github.com>
2023-05-29 23:13:41 +0200

    change len them

--- src/account/change_theme.php
+++ src/account/change_theme.php
@@ -3,24 +3,24 @@
 require_once('../lib/accounts/user.php');
 require_once('../lib/database.php');
 require_once('../lib/jwt.php');
-require_once('../lib/redirect_to.php');
 
 try {
-    $lang = $_GET['lang'];
-
-    // validate_language($lang); //
+    $theme = $_GET['theme'];
 
     $jwt_payload = validate_jwt();
 
+    if (!is_valid_map_theme($theme)) {
+        //TODO da cambiare in 400 bad request
+        throw new Exception("Invalid map theme: $theme");
+    }
+
     $conn = connect_to_database();
-    change_language($conn, $jwt_payload->email, $lang);
-
-    try_redirect();
-
-    header('Location: /');
+    update_html_theme($conn, $jwt_payload->email, $theme);
 } catch (Exception $e) {
     http_response_code(500);
     error_log("ERROR: change_language.php: " . $e->getMessage());
     echo "500 Internal Server Error";
     exit;
 }
+
+?>
